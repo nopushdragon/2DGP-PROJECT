132b5e3dbbdbe1629c8682c2467564ed13f0a0f0
YANG HYEON BIN <iks05280204@gmail.com>
2025-09-27 03:04:51 +0900

    델타 타임

--- PROJECT.py
+++ PROJECT.py
@@ -1,1 +1,17 @@
 from pico2d import *
+
+open_canvas(1200,800)
+
+running = True
+prev_time = get_time()
+
+while running:
+    curr_time = get_time()
+    dt = curr_time - prev_time
+    prev_time = curr_time
+
+    clear_canvas()
+
+    update_canvas()
+
+close_canvas()
